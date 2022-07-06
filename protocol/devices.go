95c16406770b59c307cad0ba5dd74ba2a0192450
ilya-korotya <officalbatya@gmail.com>
2022-07-06 21:12:40 +0300

    generator for protocol message

--- protocol/devices.go
+++ protocol/devices.go
@@ -8,7 +8,6 @@ const (
 	// RegisterDeviceResponseMessageType defines register device response type of the communication protocol
 	RegisterDeviceResponseMessageType iden3comm.ProtocolMessage = iden3comm.Iden3Protocol + "device/1.0/register-response"
 )
-// TODO(illia-korotia): add generator for these structures.
 
 // RegisterDeviceRequestMessage represent Iden3message for register device request
 type RegisterDeviceRequestMessage struct {
