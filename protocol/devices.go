e35a30dbbc4bc368dd2f340e67405ff013033315
ilya-korotya <officalbatya@gmail.com>
2022-07-07 16:03:04 +0300

    rename devices messages

--- protocol/devices.go
+++ protocol/devices.go
@@ -3,25 +3,25 @@ package protocol
 import "github.com/iden3/iden3comm"
 
 const (
-	// RegisterDeviceRequestMessageType defines register device request type of the communication protocol
-	RegisterDeviceRequestMessageType iden3comm.ProtocolMessage = iden3comm.Iden3Protocol + "devices/1.0/registration"
+	// DeviceRegistrationRequestMessageType defines register device request type of the communication protocol
+	DeviceRegistrationRequestMessageType iden3comm.ProtocolMessage = iden3comm.Iden3Protocol + "devices/1.0/registration"
 )
 
-// RegisterDeviceRequestMessage represent Iden3message for register device request
-type RegisterDeviceRequestMessage struct {
+// DeviceRegistrationRequestMessage represent Iden3message for register device request
+type DeviceRegistrationRequestMessage struct {
 	ID       string                    `json:"id"`
 	Typ      iden3comm.MediaType       `json:"typ,omitempty"`
 	Type     iden3comm.ProtocolMessage `json:"type"`
 	ThreadID string                    `json:"thid,omitempty"`
 
-	Body RegisterDeviceRequestMessageBody `json:"body,omitempty"`
+	Body DeviceRegistrationRequestMessageBody `json:"body,omitempty"`
 
 	From string `json:"from,omitempty"`
 	To   string `json:"to,omitempty"`
 }
 
-// RegisterDeviceRequestMessageBody is struct the represents body for register device request request
-type RegisterDeviceRequestMessageBody struct {
+// DeviceRegistrationRequestMessageBody is struct the represents body for register device request request
+type DeviceRegistrationRequestMessageBody struct {
 	AppID  string `json:"app_id"`
 	PushToken  string `json:"push_token"`
 }
