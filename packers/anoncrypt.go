510220e99ca9863a9ca7b6051141c5f99ce28fa9
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-08-12 12:29:07 +0300

    add anoncrypt

--- packers/anoncrypt.go
+++ packers/anoncrypt.go
@@ -1,12 +1,10 @@
 package packers
 
 import (
-	"crypto/ecdsa"
 	"encoding/json"
-
 	"github.com/iden3/iden3comm"
-
 	"github.com/pkg/errors"
+
 	jose "gopkg.in/square/go-jose.v2"
 )
 
@@ -14,15 +12,41 @@ import (
 const MediaTypeEncryptedMessage iden3comm.MediaType = "application/iden3comm-encrypted-json"
 
 // AnoncryptPacker is  packer for anon encryption / decryption
-type AnoncryptPacker struct{}
+type AnoncryptPacker struct {
+	kr KeyResolverHandlerFunc
+}
+
+// AnoncryptPackerParams is params for anoncrypt packer
+type AnoncryptPackerParams struct {
+	RecipientKey *jose.JSONWebKey
+	iden3comm.PackerParams
+}
+
+// NewAnoncryptPacker returns new anon packers
+func NewAnoncryptPacker(kr KeyResolverHandlerFunc) *AnoncryptPacker {
+	return &AnoncryptPacker{kr: kr}
+}
+
+// KeyResolverHandlerFunc resolve private key by key id
+type KeyResolverHandlerFunc func(keyID string) (key interface{}, err error)
+
+// Resolve function is responsible to call provided handler for recipient private key resolve
+func (kr KeyResolverHandlerFunc) Resolve(keyID string) (key interface{}, err error) {
+	return kr(keyID)
+}
 
 // Pack returns packed message to transport envelope
-func (p *AnoncryptPacker) Pack(payload []byte, receiverPubKey *ecdsa.PublicKey) ([]byte, error) {
+func (p *AnoncryptPacker) Pack(payload []byte, params iden3comm.PackerParams) ([]byte, error) {
+
+	packerParams, ok := params.(AnoncryptPackerParams)
+	if !ok {
+		return nil, errors.New("can't cast params to anoncrypt packer params")
+	}
 
 	encryptor, err := jose.NewEncrypter(jose.A256CBC_HS512, jose.Recipient{
 		Algorithm: jose.ECDH_ES_A256KW,
-		Key:       receiverPubKey,
-		KeyID:     "kid",
+		Key:       packerParams.RecipientKey,
+		KeyID:     packerParams.RecipientKey.KeyID,
 	}, new(jose.EncrypterOptions).WithType(jose.ContentType(MediaTypeEncryptedMessage)))
 	if err != nil {
 		return nil, err
@@ -42,10 +66,22 @@ func (p *AnoncryptPacker) Pack(payload []byte, receiverPubKey *ecdsa.PublicKey) 
 // Unpack returns unpacked message from transport envelope
 func (p *AnoncryptPacker) Unpack(envelope []byte) (*iden3comm.BasicMessage, error) {
 
-	var msg iden3comm.BasicMessage
-	err := json.Unmarshal(envelope, &msg)
+	jwe, err := jose.ParseEncrypted(string(envelope))
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, err
+	}
+	decryptionKey, err := p.kr.Resolve(jwe.Header.KeyID)
+	if err != nil {
+		return nil, err
+	}
+	payload, err := jwe.Decrypt(decryptionKey)
+	if err != nil {
+		return nil, err
+	}
+	var msg iden3comm.BasicMessage
+	err = json.Unmarshal(payload, &msg)
+	if err != nil {
+		return nil, err
 	}
 	return &msg, err
 }
