a9e461714acac79a6147a039e6e485201e7798e7
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2023-04-13 16:43:04 +0300

    add resolver handler

--- packers/jws.go
+++ packers/jws.go
@@ -3,6 +3,7 @@ package packers
 import (
 	"encoding/json"
 
+	"github.com/iden3/go-schema-processor/verifiable"
 	"github.com/iden3/iden3comm"
 	"github.com/pkg/errors"
 	"gopkg.in/square/go-jose.v2"
@@ -11,12 +12,24 @@ import (
 // MediaTypeSignedMessage is media type for jws
 const MediaTypeSignedMessage iden3comm.MediaType = "application/iden3-signed-json"
 
+// DIDResolverHandlerFunc resolves did
+type DIDResolverHandlerFunc func(did string) (verifiable.DIDDocument, error)
+
+// Resolve function is responsible to call provided handler for resolve did document
+func (f DIDResolverHandlerFunc) Resolve(did string) (verifiable.DIDDocument, error) {
+	return f(did)
+}
+
 // JWSPacker is packer that use jws
 type JWSPacker struct {
+	didResolverHandler DIDResolverHandlerFunc
+	keyResolverHandler KeyResolverHandlerFunc
 }
 
 // SigningParams packer parameters for jws generation
 type SigningParams struct {
+	SenderDID string
+	Alg       jose.SignatureAlgorithm
 	iden3comm.PackerParams
 }
 
@@ -26,8 +39,8 @@ func NewSigningParams() SigningParams {
 }
 
 // NewJWSPacker creates new jws packer instance
-func NewJWSPacker() *JWSPacker {
-	return &JWSPacker{}
+func NewJWSPacker(didResolverHandler DIDResolverHandlerFunc, keyResolverHandlerFunc KeyResolverHandlerFunc) *JWSPacker {
+	return &JWSPacker{didResolverHandler, keyResolverHandlerFunc}
 }
 
 // Pack returns packed message to transport envelope with a zero knowledge proof in JWS full serialized format
@@ -37,11 +50,50 @@ func (p *JWSPacker) Pack(payload []byte, params iden3comm.PackerParams) ([]byte,
 	var err error
 	var token *jose.JSONWebSignature
 
-	_, ok := params.(SigningParams)
+	signingParams, ok := params.(SigningParams)
 	if !ok {
 		return nil, errors.New("can't cast params to signer packer params")
 	}
 
+	bm := &iden3comm.BasicMessage{}
+	err = json.Unmarshal(payload, &bm)
+	if err != nil {
+		return nil, errors.New("msg singer must me be msg sender")
+	}
+
+	if bm.From != signingParams.SenderDID {
+		return nil, errors.New("msg singer must me be msg sender")
+	}
+
+	// 1. resolver did doc
+
+	_, err = p.didResolverHandler.Resolve(signingParams.SenderDID)
+
+	if err != nil {
+		return nil, errors.New("can't resolve did")
+	}
+
+	// 2. parse verification method
+
+	const kid = ""
+
+	key, err := p.keyResolverHandler.Resolve(kid)
+	if err != nil {
+		return nil, errors.New("can't resolve key")
+	}
+	// 3. sign with ...
+
+	// This time, sign and do not embed JWK in message
+	signer, err := jose.NewSigner(jose.SigningKey{Algorithm: jose.ES256, Key: key}, nil)
+	if err != nil {
+		return nil, errors.New("can't create signer")
+	}
+
+	token, err = signer.Sign(payload)
+	if err != nil {
+		return nil, errors.New("can't sign")
+
+	}
 	return []byte(token.FullSerialize()), err
 }
 
--- packers/jws_test.go
+++ packers/jws_test.go
@@ -1,30 +1,33 @@
 package packers
 
 import (
+	"crypto/ecdsa"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"testing"
 
+	"github.com/iden3/go-schema-processor/verifiable"
 	"github.com/iden3/iden3comm/protocol"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestJWSPacker_Pack(t *testing.T) {
 
-	p := NewJWSPacker()
-
+	p := setupJWSPacker()
 	msgBytes := []byte(`{"type":"https://iden3-communication.io/authorization/1.0/response","from":"did:iden3:polygon:mumbai:x4jcHP4XHTK3vX58AHZPyHE8kYjneyE6FZRfz7K29","body":{"scope":[{"type":"zeroknowledge","circuit_id":"auth","pub_signals":["1","18311560525383319719311394957064820091354976310599818797157189568621466950811","323416925264666217617288569742564703632850816035761084002720090377353297920"],"proof_data":{"pi_a":["11130843150540789299458990586020000719280246153797882843214290541980522375072","1300841912943781723022032355836893831132920783788455531838254465784605762713","1"],"pi_b":[["20615768536988438336537777909042352056392862251785722796637590212160561351656","10371144806107778890538857700855108667622042215096971747203105997454625814080"],["19598541350804478549141207835028671111063915635580679694907635914279928677812","15264553045517065669171584943964322117397645147006909167427809837929458012913"],["1","0"]],"pi_c":["16443309279825508893086251290003936935077348754097470818523558082502364822049","2984180227766048100510120407150752052334571876681304999595544138155611963273","1"],"protocol":""}}]}}`)
 
 	_ = "did:iden3:polygon:mumbai:x4jcHP4XHTK3vX58AHZPyHE8kYjneyE6FZRfz7K29"
 
-	_, err := p.Pack(msgBytes, SigningParams{})
+	_, err := p.Pack(msgBytes, SigningParams{SenderDID: `did:iden3:polygon:mumbai:x4jcHP4XHTK3vX58AHZPyHE8kYjneyE6FZRfz7K29`})
 	assert.Nil(t, err)
 
 }
 
 func TestJWSPacker_Unpack(t *testing.T) {
 
-	p := NewJWSPacker()
+	p := setupJWSPacker()
 
 	msgZKP := []byte(`eyJhbGciOiJFUzI1NksiLCJraWQiOiJkaWQ6ZXhhbXBsZToxMjMjdm0tMSIsInR5cCI6ImFwcGxpY2F0aW9uL2lkZW4zY29tbS1zaWduZWQtanNvbiJ9.eyJ0eXBlIjoiaHR0cHM6Ly9pZGVuMy1jb21tdW5pY2F0aW9uLmlvL2F1dGhvcml6YXRpb24vMS4wL3Jlc3BvbnNlIiwiZnJvbSI6ImRpZDpleGFtcGxlOjEyMyIsImJvZHkiOnsic2NvcGUiOlt7InR5cGUiOiJ6ZXJva25vd2xlZGdlIiwiY2lyY3VpdF9pZCI6ImF1dGgiLCJwdWJfc2lnbmFscyI6WyIxIiwiMTgzMTE1NjA1MjUzODMzMTk3MTkzMTEzOTQ5NTcwNjQ4MjAwOTEzNTQ5NzYzMTA1OTk4MTg3OTcxNTcxODk1Njg2MjE0NjY5NTA4MTEiLCIzMjM0MTY5MjUyNjQ2NjYyMTc2MTcyODg1Njk3NDI1NjQ3MDM2MzI4NTA4MTYwMzU3NjEwODQwMDI3MjAwOTAzNzczNTMyOTc5MjAiXSwicHJvb2ZfZGF0YSI6eyJwaV9hIjpbIjExMTMwODQzMTUwNTQwNzg5Mjk5NDU4OTkwNTg2MDIwMDAwNzE5MjgwMjQ2MTUzNzk3ODgyODQzMjE0MjkwNTQxOTgwNTIyMzc1MDcyIiwiMTMwMDg0MTkxMjk0Mzc4MTcyMzAyMjAzMjM1NTgzNjg5MzgzMTEzMjkyMDc4Mzc4ODQ1NTUzMTgzODI1NDQ2NTc4NDYwNTc2MjcxMyIsIjEiXSwicGlfYiI6W1siMjA2MTU3Njg1MzY5ODg0MzgzMzY1Mzc3Nzc5MDkwNDIzNTIwNTYzOTI4NjIyNTE3ODU3MjI3OTY2Mzc1OTAyMTIxNjA1NjEzNTE2NTYiLCIxMDM3MTE0NDgwNjEwNzc3ODg5MDUzODg1NzcwMDg1NTEwODY2NzYyMjA0MjIxNTA5Njk3MTc0NzIwMzEwNTk5NzQ1NDYyNTgxNDA4MCJdLFsiMTk1OTg1NDEzNTA4MDQ0Nzg1NDkxNDEyMDc4MzUwMjg2NzExMTEwNjM5MTU2MzU1ODA2Nzk2OTQ5MDc2MzU5MTQyNzk5Mjg2Nzc4MTIiLCIxNTI2NDU1MzA0NTUxNzA2NTY2OTE3MTU4NDk0Mzk2NDMyMjExNzM5NzY0NTE0NzAwNjkwOTE2NzQyNzgwOTgzNzkyOTQ1ODAxMjkxMyJdLFsiMSIsIjAiXV0sInBpX2MiOlsiMTY0NDMzMDkyNzk4MjU1MDg4OTMwODYyNTEyOTAwMDM5MzY5MzUwNzczNDg3NTQwOTc0NzA4MTg1MjM1NTgwODI1MDIzNjQ4MjIwNDkiLCIyOTg0MTgwMjI3NzY2MDQ4MTAwNTEwMTIwNDA3MTUwNzUyMDUyMzM0NTcxODc2NjgxMzA0OTk5NTk1NTQ0MTM4MTU1NjExOTYzMjczIiwiMSJdLCJwcm90b2NvbCI6IiJ9fV19fQ.de_qaDM7VYFaPUCNDGsvwF04tT4S4nXBO8dqXnU8XAof0Uip5LDCe4-IjEBPxu0sLh8BxcvPHMYMjx_pvPcqWw`)
 	iden3msg, err := p.Unpack(msgZKP)
@@ -38,4 +41,19 @@ func TestJWSPacker_Unpack(t *testing.T) {
 	assert.Equal(t, protocol.AuthorizationResponseMessageType, authResponse.Type)
 	assert.Len(t, authResponse.Body.Scope, 1)
 
+}
+
+func setupJWSPacker() *JWSPacker {
+	return NewJWSPacker(func(did string) (verifiable.DIDDocument, error) {
+		var doc verifiable.DIDDocument
+		_ = json.Unmarshal([]byte(`{"@context":["https://www.w3.org/ns/did/v1","https://w3id.org/security/suites/secp256k1recovery-2020/v2",{"esrs2020":"https://identity.foundation/EcdsaSecp256k1RecoverySignature2020#","privateKeyJwk":{"@id":"esrs2020:privateKeyJwk","@type":"@json"},"publicKeyHex":"esrs2020:publicKeyHex","privateKeyHex":"esrs2020:privateKeyHex","ethereumAddress":"esrs2020:ethereumAddress"}],"id":"did:example:123","verificationMethod":[{"id":"did:example:123#vm-1","controller":"did:example:123","type":"EcdsaSecp256k1VerificationKey2019","publicKeyJwk":{"crv":"secp256k1","kid":"JUvpllMEYUZ2joO59UNui_XYDqxVqiFLLAJ8klWuPBw","kty":"EC","x":"_dV63sPUOOojf-RrM-4eAW7aa1hcPifqZmhsLqU1hHk","y":"Rjk_gUUlLupor-Z-KHs-2bMWhbpsOwAGCnO5sSQtaPc"}}],"authentication":["did:example:123#vm-1"]}`), &doc)
+		return doc, nil
+	}, func(keyID string) (key interface{}, err error) {
+		bb, _ := hex.DecodeString(`278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f`)
+		k := ecdsa.PrivateKey{
+			PublicKey: ecdsa.PublicKey{},
+			D:         new(big.Int).SetBytes(bb),
+		}
+		return k, nil
+	})
 }
