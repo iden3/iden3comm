c070d4db07f3d57b9b540b83fcf485812e35916b
GopherDID <74898029+vmidyllic@users.noreply.github.com>
2025-01-28 17:41:51 +0200

    Feature/message onchain response (#74)
    * add new message Contract Invoke Response

--- protocol/contract_invoke.go
+++ protocol/contract_invoke.go
@@ -9,6 +9,8 @@ import (
 const (
 	// ContractInvokeRequestMessageType defines contract invoke request type of the communication protocol
 	ContractInvokeRequestMessageType iden3comm.ProtocolMessage = iden3comm.Iden3Protocol + "proofs/1.0/contract-invoke-request"
+	// ContractInvokeResponseMessageType defines contract invoke response type of the communication protocol
+	ContractInvokeResponseMessageType iden3comm.ProtocolMessage = iden3comm.Iden3Protocol + "proofs/1.0/contract-invoke-response"
 )
 
 // ContractInvokeRequestMessage is struct the represents iden3message contract invoke request
@@ -40,4 +42,32 @@ type TransactionData struct {
 	MethodID        string `json:"method_id"`
 	ChainID         int    `json:"chain_id"`
 	Network         string `json:"network"`
+}
+
+// ContractInvokeResponseMessage is struct the represents iden3message contract invoke response
+type ContractInvokeResponseMessage struct {
+	ID       string                            `json:"id"`
+	Typ      iden3comm.MediaType               `json:"typ,omitempty"`
+	Type     iden3comm.ProtocolMessage         `json:"type"`
+	ThreadID string                            `json:"thid,omitempty"`
+	Body     ContractInvokeResponseMessageBody `json:"body,omitempty"`
+
+	From string `json:"from,omitempty"`
+	To   string `json:"to,omitempty"`
+
+	CreatedTime *int64 `json:"created_time,omitempty"`
+	ExpiresTime *int64 `json:"expires_time,omitempty"`
+}
+
+// ContractInvokeResponseMessageBody is body for contract invoke response
+type ContractInvokeResponseMessageBody struct {
+	TransactionData TransactionData                     `json:"transaction_data"`
+	DIDDoc          json.RawMessage                     `json:"did_doc,omitempty"`
+	Scope           []OnchainZeroKnowledgeProofResponse `json:"scope"`
+}
+
+// OnchainZeroKnowledgeProofResponse represents structure of zkp response given for onchain verification
+type OnchainZeroKnowledgeProofResponse struct {
+	ZeroKnowledgeProofResponse
+	TxHash string `json:"txHash"`
 }
