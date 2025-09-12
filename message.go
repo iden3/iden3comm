96c737775f8dad220ad0e4d1d0c493c05107bb65
Dimasik Kolezhniuk <dmytro.k@privado.id>
2025-09-12 16:15:18 +0700

    attachments field to the protocol (#73)
    * Add  attachments field to the protocol
    
    
    ---------
    
    Co-authored-by: Vladyslav Munin <vladcisco.vm@gmail.com>

--- message.go
+++ message.go
@@ -18,6 +18,8 @@ type BasicMessage struct {
 
 	CreatedTime *int64 `json:"created_time,omitempty"`
 	ExpiresTime *int64 `json:"expires_time,omitempty"`
+
+	Attachments []Attachment `json:"attachments,omitempty"`
 }
 
 // ProtocolMessage is IDEN3Comm message
